eb293134aa45694151ad1601b85d70bfe8e5637a
fideloper <fideloper@gmail.com>
2015-05-25 20:41:08 -0500

    provisioning with ansible

--- provision.sh
+++ provision.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-# Read in Ansible and start provisioning from `/home/vagrant/ops/ansible`
+# Head to copied Ansible files
+cd /home/vagrant/ops/ansible
+
+# Provision with Ansible
+ansible-playbook -s \
+                 -i ./hosts
+                 ./config.yml
